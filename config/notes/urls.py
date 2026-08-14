fdc620d68c952dc6b6670bbbc507d3cb96a665b1
RomahaPoleha <romashkoo2023@list.ru>
2026-08-14 12:59:34 +1000

    fix: use get_object_or_404 in note detail view

--- config/notes/urls.py
+++ config/notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.note_list, name='note_list'),
+    path("note/<int:pk>/", views.note_detail, name = "note_detail")
 ]
--- config/notes/views.py
+++ config/notes/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Note
 
 
 def note_list(request):
     notes = Note.objects.all()
-    return render(request, "notes/note_list.html", {"notes":notes})
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/note_detail.html', {'note': note})
